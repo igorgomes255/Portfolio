3cbf58c0ca87839f8ce831cfefc18eae7b741413
Igor Gomes <igorgomes255@gmail.com>
2023-01-18 14:57:43 -0300

    feat: adicionando portfolio

---
+++ src/utils/stackData.ts
@@ -0,0 +1,55 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiDjango,
+  SiExpress,
+  SiPostgresql,
+  SiStyledcomponents,
+  SiTypescript,
+} from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+} from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  { title: "PostgreSQL", img: SiPostgresql },
+  {
+    title: "Express JS",
+    img: SiExpress,
+  },
+
+  { title: "TypeScript", img: SiTypescript },
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "Styled Components",
+    img: SiStyledcomponents,
+  },
+
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "Docker", img: FaDocker },
+  { title: "Python", img: FaPython },
+  {
+    title: "Django",
+    img: SiDjango,
+  },
+  { title: "Git", img: FaGit },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Igor Gomes",
+  githubUser: "igorgomes255",
+  linkedinUser: "igor-gomes-ab8b72246",
+  whatsappNumber: "+5562984588105",
+  emailUser: "igorgomes255@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
